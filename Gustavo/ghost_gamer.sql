f29147f0adc858846099556fd19997bd4f4ad318
gustav2308 <gustavoxmoreira2008@gmail.com>
2026-06-16 14:33:19 -0300

    banco com tabela empresa e coluna adm

--- Gustavo/ghost_gamer.sql
+++ Gustavo/ghost_gamer.sql
@@ -1,94 +1,123 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- version 4.1.4
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19/05/2026 às 18:50
--- Versão do servidor: 10.4.28-MariaDB
--- Versão do PHP: 8.2.4
+-- Generation Time: 16-Jun-2026 às 19:31
+-- Versão do servidor: 5.6.15-log
+-- PHP Version: 5.5.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Banco de dados: `ghost_gamer`
+-- Database: `ghost_gamer`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `clientes`
+-- Estrutura da tabela `clientes`
 --
 
-CREATE TABLE `clientes` (
-  `id_cliente` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
   `cli_nome` varchar(110) DEFAULT NULL,
   `email` varchar(110) DEFAULT NULL,
   `nickname` varchar(110) DEFAULT NULL,
-  `senha` varchar(30) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `senha` varchar(255) DEFAULT NULL,
+  `administrador` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id_cliente`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
 
 --
--- Despejando dados para a tabela `clientes`
+-- Extraindo dados da tabela `clientes`
 --
 
-INSERT INTO `clientes` (`id_cliente`, `cli_nome`, `email`, `nickname`, `senha`) VALUES
-(1, 'Rogério Gomes Ferreira', 'Rogersferreira@gmail.com', 'RogerGigaChad', 'banana12345'),
-(2, 'Patrícia Borges Cardoso', 'pbclinda@gmail.com', 'PapaBorges', 'tenis12345'),
-(3, 'Tonio Marcelo Rossi', 'rossitoni@gmail.com', 'Rossi', '22334455'),
-(4, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '22002200'),
-(5, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '23/08/2008'),
-(6, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '11112233'),
-(7, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '11112233'),
-(8, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '11112233'),
-(9, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '333333333'),
-(10, 'juju', 'cecececececec@gmail', 'sdadfwefwf', '44444444');
+INSERT INTO `clientes` (`id_cliente`, `cli_nome`, `email`, `nickname`, `senha`, `administrador`) VALUES
+(1, 'Rogério Gomes Ferreira', 'Rogersferreira@gmail.com', 'RogerGigaChad', 'banana12345', 0),
+(2, 'Patrícia Borges Cardoso', 'pbclinda@gmail.com', 'PapaBorges', 'tenis12345', 0),
+(3, 'Tonio Marcelo Rossi', 'rossitoni@gmail.com', 'Rossi', '22334455', 0),
+(4, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '22002200', 0),
+(5, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '23/08/2008', 0),
+(6, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '11112233', 0),
+(7, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '11112233', 0),
+(8, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '11112233', 0),
+(9, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '333333333', 0),
+(10, 'juju', 'cecececececec@gmail', 'sdadfwefwf', '44444444', 0),
+(11, 'Pericles', 'edmundo@gmail.com', 'Jusimar', '987654321', 0),
+(12, 'Pericles', 'edmundo@gmail.com', 'Jusimar', '$2y$10$HcVzRFbkfGBglJ0U9T.zouVdMEwsBoNSilQGYnf3t/US.yA15sO4C', 0);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `gpu`
+-- Estrutura da tabela `empresa`
 --
 
-CREATE TABLE `gpu` (
-  `id_gpu` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `empresa` (
+  `id_empresa` int(11) NOT NULL AUTO_INCREMENT,
+  `razao_social` varchar(155) NOT NULL,
+  `nome_fantasia` varchar(150) NOT NULL,
+  `CNPJ` varchar(14) NOT NULL,
+  `data_abertura` date DEFAULT NULL,
+  `telefone` varchar(14) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `Rua` varchar(100) DEFAULT NULL,
+  `numero` varchar(10) DEFAULT NULL,
+  `bairro` varchar(80) DEFAULT NULL,
+  `cidade` varchar(100) DEFAULT NULL,
+  `estado` char(2) DEFAULT NULL,
+  `cep` char(8) DEFAULT NULL,
+  `pais` char(2) DEFAULT NULL,
+  PRIMARY KEY (`id_empresa`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `gpu`
+--
+
+CREATE TABLE IF NOT EXISTS `gpu` (
+  `id_gpu` int(11) NOT NULL AUTO_INCREMENT,
   `modelo` varchar(40) DEFAULT NULL,
   `marca` varchar(20) DEFAULT NULL,
   `fabricante` varchar(20) DEFAULT NULL,
-  `VRAM` int(10) UNSIGNED DEFAULT NULL,
+  `VRAM` int(10) unsigned DEFAULT NULL,
   `tipo_memoria` varchar(20) DEFAULT NULL,
-  `clock` int(10) UNSIGNED DEFAULT NULL,
-  `boost` int(10) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `clock` int(10) unsigned DEFAULT NULL,
+  `boost` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id_gpu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `jogo`
+-- Estrutura da tabela `jogo`
 --
 
-CREATE TABLE `jogo` (
-  `id_jogo` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `jogo` (
+  `id_jogo` int(11) NOT NULL AUTO_INCREMENT,
   `titulo` varchar(100) NOT NULL,
   `empresa_email` varchar(110) DEFAULT NULL,
   `genero` varchar(110) DEFAULT NULL,
-  `nucleos` int(10) UNSIGNED DEFAULT NULL,
-  `threads` int(10) UNSIGNED DEFAULT NULL,
+  `nucleos` int(10) unsigned DEFAULT NULL,
+  `threads` int(10) unsigned DEFAULT NULL,
   `frequencia` decimal(10,0) DEFAULT NULL,
-  `ram_gb` int(10) UNSIGNED DEFAULT NULL,
-  `vram_gb` int(10) UNSIGNED DEFAULT NULL,
-  `armazenamento` int(10) UNSIGNED DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `ram_gb` int(10) unsigned DEFAULT NULL,
+  `vram_gb` int(10) unsigned DEFAULT NULL,
+  `armazenamento` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id_jogo`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
--- Despejando dados para a tabela `jogo`
+-- Extraindo dados da tabela `jogo`
 --
 
 INSERT INTO `jogo` (`id_jogo`, `titulo`, `empresa_email`, `genero`, `nucleos`, `threads`, `frequencia`, `ram_gb`, `vram_gb`, `armazenamento`) VALUES
@@ -99,105 +128,38 @@ INSERT INTO `jogo` (`id_jogo`, `titulo`, `empresa_email`, `genero`, `nucleos`, `
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `memoria_ram`
+-- Estrutura da tabela `memoria_ram`
 --
 
-CREATE TABLE `memoria_ram` (
-  `id_ram` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `memoria_ram` (
+  `id_ram` int(11) NOT NULL AUTO_INCREMENT,
   `MODELO` varchar(40) DEFAULT NULL,
   `MARCA` varchar(20) DEFAULT NULL,
-  `CAPACIDADE` int(10) UNSIGNED DEFAULT NULL,
+  `CAPACIDADE` int(10) unsigned DEFAULT NULL,
   `TIPO` varchar(20) DEFAULT NULL,
-  `FREQUENCIA` int(10) UNSIGNED DEFAULT NULL,
-  `MODULOS` int(10) UNSIGNED DEFAULT NULL,
-  `LATENCIA` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `FREQUENCIA` int(10) unsigned DEFAULT NULL,
+  `MODULOS` int(10) unsigned DEFAULT NULL,
+  `LATENCIA` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id_ram`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `processador`
+-- Estrutura da tabela `processador`
 --
 
-CREATE TABLE `processador` (
-  `id_processador` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `processador` (
+  `id_processador` int(11) NOT NULL AUTO_INCREMENT,
   `modelo` varchar(40) DEFAULT NULL,
   `marca` varchar(20) DEFAULT NULL,
   `geracao` varchar(20) DEFAULT NULL,
-  `nucleo` int(10) UNSIGNED DEFAULT NULL,
-  `threads` int(10) UNSIGNED DEFAULT NULL,
+  `nucleo` int(10) unsigned DEFAULT NULL,
+  `threads` int(10) unsigned DEFAULT NULL,
   `clock_base` decimal(4,2) DEFAULT NULL,
-  `boost` decimal(4,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `clientes`
---
-ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`id_cliente`);
-
---
--- Índices de tabela `gpu`
---
-ALTER TABLE `gpu`
-  ADD PRIMARY KEY (`id_gpu`);
-
---
--- Índices de tabela `jogo`
---
-ALTER TABLE `jogo`
-  ADD PRIMARY KEY (`id_jogo`);
-
---
--- Índices de tabela `memoria_ram`
---
-ALTER TABLE `memoria_ram`
-  ADD PRIMARY KEY (`id_ram`);
-
---
--- Índices de tabela `processador`
---
-ALTER TABLE `processador`
-  ADD PRIMARY KEY (`id_processador`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `clientes`
---
-ALTER TABLE `clientes`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT de tabela `gpu`
---
-ALTER TABLE `gpu`
-  MODIFY `id_gpu` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `jogo`
---
-ALTER TABLE `jogo`
-  MODIFY `id_jogo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT de tabela `memoria_ram`
---
-ALTER TABLE `memoria_ram`
-  MODIFY `id_ram` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `processador`
---
-ALTER TABLE `processador`
-  MODIFY `id_processador` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
+  `boost` decimal(4,2) DEFAULT NULL,
+  PRIMARY KEY (`id_processador`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
