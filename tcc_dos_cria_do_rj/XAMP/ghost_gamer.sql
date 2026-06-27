d4a25ac25353c0d2de69a815e1b95a19f0488b15
gustav2308 <gustavoxmoreira2008@gmail.com>
2026-06-26 22:22:20 -0300

    Banco de dados atualizado
    3 Contas admins, mais de 4 usuários cadastrados;
    3 Empresas cadastradas;

--- tcc_dos_cria_do_rj/XAMP/ghost_gamer.sql
+++ tcc_dos_cria_do_rj/XAMP/ghost_gamer.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 16-Jun-2026 às 19:31
+-- Generation Time: 27-Jun-2026 às 03:20
 -- Versão do servidor: 5.6.15-log
 -- PHP Version: 5.5.8
 
@@ -32,27 +32,24 @@ CREATE TABLE IF NOT EXISTS `clientes` (
   `email` varchar(110) DEFAULT NULL,
   `nickname` varchar(110) DEFAULT NULL,
   `senha` varchar(255) DEFAULT NULL,
-  `administrador` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`id_cliente`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+  `administrador` enum('usuario','admin') NOT NULL DEFAULT 'usuario',
+  PRIMARY KEY (`id_cliente`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=24 ;
 
 --
 -- Extraindo dados da tabela `clientes`
 --
 
 INSERT INTO `clientes` (`id_cliente`, `cli_nome`, `email`, `nickname`, `senha`, `administrador`) VALUES
-(1, 'Rogério Gomes Ferreira', 'Rogersferreira@gmail.com', 'RogerGigaChad', 'banana12345', 0),
-(2, 'Patrícia Borges Cardoso', 'pbclinda@gmail.com', 'PapaBorges', 'tenis12345', 0),
-(3, 'Tonio Marcelo Rossi', 'rossitoni@gmail.com', 'Rossi', '22334455', 0),
-(4, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '22002200', 0),
-(5, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '23/08/2008', 0),
-(6, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '11112233', 0),
-(7, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '11112233', 0),
-(8, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '11112233', 0),
-(9, 'dvecvececec', 'cecececececec@gmail', 'sdadfwefwf', '333333333', 0),
-(10, 'juju', 'cecececececec@gmail', 'sdadfwefwf', '44444444', 0),
-(11, 'Pericles', 'edmundo@gmail.com', 'Jusimar', '987654321', 0),
-(12, 'Pericles', 'edmundo@gmail.com', 'Jusimar', '$2y$10$HcVzRFbkfGBglJ0U9T.zouVdMEwsBoNSilQGYnf3t/US.yA15sO4C', 0);
+(14, 'Gustavo Almeida', 'gustavo.almeida@gmail.com', 'GhostGus', '$2y$10$pc9O/jaoot.oVYtu.0geN.sx1FMI07mfH74aZNjpHT1LOic7Q1j1.', 'usuario'),
+(15, 'Marina Oliveira', 'marina.oliveira@gmail.com', 'MariGames', '$2y$10$ZqxVhGqgVTuuAVXruPnjkOcJcUecjOzqFf7UcJrGxIByfLZvUGwF2', 'usuario'),
+(16, 'Camila Rodrigues', 'camila.rodrigues@gmail.com', 'KamiGG', '$2y$10$ExaW.Jiogt/tGzAU5jpZK.Oh/asT.d3p6BSr8m7tMFarZ1F22yD7S', 'usuario'),
+(17, 'Lucas Ferreira', 'lucas.ferreira@gmail.com', 'LkzFPS', '$2y$10$LECIQz5mnKpH0Sz9HMYeg.JRFZxdSD6aKPXGL7DDtS7sX0TMaeZgC', 'usuario'),
+(18, 'Rafael Santos', 'rafael.santos@gmail.com', 'RafaX', '$2y$10$5eMUe2ickq8E9j6gDNlakuIHurKzuVqUvfSiP7Q7yOa2DakH6zmPG', 'usuario'),
+(21, 'Gustavo Moreira', 'gustavoxmoreira2008@gmail.com', 'Gus', '$2y$10$neGXY7/rNYxeB.1SVDYT4.B8u7wqmlfslQjkGd5IcUAMW6teHQWFm', 'admin'),
+(22, 'Lucas Santos', 'lucasberlucasbernardinellisan@gmail.com', 'Havyto', '$2y$10$hQ7alZJTarojmKzxIqq0WuqSmPBNGw.Aiz1XJbdpYDgZkp8YxCN7i', 'admin'),
+(23, 'Lucas Magalhaes', 'lucas.magalhaes31102008@gmail.com', 'Lucas', '$2y$10$bAcdoLDjrbZxrPgvzyrehOpGUbTqAn0PvJcZ1QYewJis.1KWh309q', 'admin');
 
 -- --------------------------------------------------------
 
@@ -76,7 +73,16 @@ CREATE TABLE IF NOT EXISTS `empresa` (
   `cep` char(8) DEFAULT NULL,
   `pais` char(2) DEFAULT NULL,
   PRIMARY KEY (`id_empresa`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Extraindo dados da tabela `empresa`
+--
+
+INSERT INTO `empresa` (`id_empresa`, `razao_social`, `nome_fantasia`, `CNPJ`, `data_abertura`, `telefone`, `email`, `Rua`, `numero`, `bairro`, `cidade`, `estado`, `cep`, `pais`) VALUES
+(1, 'TechNova Games Ltda', 'Ghost Gamer Studios', '12.345.678/000', '2022-08-12', '(11) 98765-432', 'contato@ghostgamer.com', 'Rua das Gameplays', '245', 'Centro', 'SÃ£o Paulo', 'SP', '01001-00', 'Br'),
+(2, 'PixelStorm Tecnologia Digital Ltda', 'PixelStorm Games', '98.765.432/000', '2012-05-03', '(21) 97654-321', 'suporte@pixelstorm.com', 'Avenida Digital World', '880', 'Vila TecnolÃ³gica', 'Rio de Janeiro', 'RJ', '20040-02', 'Br'),
+(3, 'NeoArcade Entertainment S.A.', 'NeoArcade', '45.678.901/000', '2002-11-28', '(31) 99876-543', 'contato@neoarcade.com', 'Rua dos E-Sports', '1020', 'Savassi', 'Belo Horizonte', 'MG', '30140-11', 'Br');
 
 -- --------------------------------------------------------
 
