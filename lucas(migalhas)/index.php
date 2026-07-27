df8a0a1c0b06d89fd677ac5b6dc9fca6f96999b0
gustav2308 <gustavoxmoreira2008@gmail.com>
2026-07-27 14:36:18 -0300

    Add files via upload

--- lucas(migalhas)/index.php
+++ lucas(migalhas)/index.php
@@ -35,23 +35,23 @@ $tipo = isset($_SESSION["administrador"]) ? $_SESSION["administrador"] : "usuari
     <nav id="menu" class="menu">
         <ul>
             <li><a href="index.php">Home</a></li>
-            <li><a href="PAGINAS/biblioteca.html">Biblioteca</a></li>
-            <li><a href="PAGINAS/categoria.html">Categoria</a></li>
+            <li><a href="PAGINAS/biblioteca.php">Biblioteca</a></li>
+            <li><a href="PAGINAS/categoria.php">Categoria</a></li>
 
             <!-- ADMIN ONLY -->
             <?php if ($tipo === "admin") { ?>
                 <li><a href="XAMP/consulta.php">Consulta</a></li>
                 <li><a href="XAMP/cadastroEmpresa.php">Cadastro de Empresa</a></li>
-                <li><a href="PAGINAS/cadastroJogos.html">Cadastro de Jogos</a></li>
+                <li><a href="PAGINAS/cadastroJogos.php">Cadastro de Jogos</a></li>
                 <li><a href="XAMP/alteraCliente.php">Altera Cliente</a></li>
                 <li><a href="XAMP/alteraEmpresa.php">Altera Empresa</a></li>
-                <li><a href="XAMP/alteraJogo.php">Altera Jogo</a></li>
+                <li><a href="alteraJogo.php">Altera Jogo</a></li>
             <?php } ?>
 
-            <li><a href="PAGINAS/perfil.html">Perfil</a></li>
-            <li><a href="PAGINAS/suporte.html">Suporte</a></li>
+            <li><a href="PAGINAS/perfil.php">Perfil</a></li>
+            <li><a href="PAGINAS/suporte.php">Suporte</a></li>
             <li><a href="PAGINAS/loginCliente.html">Login</a></li>
-            <li><a href="PAGINAS/cadastroCliente.html">Cadastro</a></li>
+            <li><a href="PAGINAS/cadastroCliente.php">Cadastro</a></li>
             <li><a href="XAMP/logout.php">SAIR</a></li>
 
         </ul>
